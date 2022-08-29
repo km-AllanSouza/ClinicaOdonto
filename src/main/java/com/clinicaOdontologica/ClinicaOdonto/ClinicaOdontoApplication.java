e5b5a1c4744f66823dd2c6f067a326919cca79b1
Vinicius <mrVin.dev@gmail.com>
2022-08-29 19:44:46 -0400

    initial commit

--- src/main/java/com/clinicaOdontologica/ClinicaOdonto/ClinicaOdontoApplication.java
+++ src/main/java/com/clinicaOdontologica/ClinicaOdonto/ClinicaOdontoApplication.java
@@ -3,6 +3,13 @@ package com.clinicaOdontologica.ClinicaOdonto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+//private static final Logger logger = LogManager.getLogger(Main.class);
+
+//import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+
 @SpringBootApplication
 public class ClinicaOdontoApplication {
 
