2b7b81b204bcab9982a96cc00b6ec8e08ca69e2a
Vinicius <mrVin.dev@gmail.com>
2022-09-06 20:13:15 -0400

    implementacao classe consulta

---
+++ src/main/java/com/clinicaOdontologica/ClinicaOdonto/repository/ConsultaRepository.java
@@ -0,0 +1,7 @@
+package com.clinicaOdontologica.ClinicaOdonto.repository;
+
+import com.clinicaOdontologica.ClinicaOdonto.model.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+}
--- src/main/java/com/clinicaOdontologica/ClinicaOdonto/service/ConsultaService.java
+++ src/main/java/com/clinicaOdontologica/ClinicaOdonto/service/ConsultaService.java
@@ -1,48 +1,41 @@
 package com.clinicaOdontologica.ClinicaOdonto.service;
 
+import com.clinicaOdontologica.ClinicaOdonto.model.Consulta;
 import com.clinicaOdontologica.ClinicaOdonto.model.Paciente;
 import com.clinicaOdontologica.ClinicaOdonto.model.dto.PacienteDTO;
+import com.clinicaOdontologica.ClinicaOdonto.repository.ConsultaRepository;
 import com.clinicaOdontologica.ClinicaOdonto.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class PacienteService {
-
+public class ConsultaService {
     @Autowired
-    PacienteRepository repository;
+    ConsultaRepository repository;
 
-    public Paciente salvar(Paciente paciente){
-        return repository.save(paciente);
+    public Consulta salvar(Consulta consulta){
+        return repository.save(consulta);
     }
 
-    public List<PacienteDTO> buscarTodos(){
-        List<Paciente> pacienteList = repository.findAll();
-        List<PacienteDTO> pacienteDTOList = new ArrayList<>();
+    public List<Consulta> buscarTodos(){
+        List<Consulta> consultaList = repository.findAll();
 
-        for(Paciente e : pacienteList){
-            pacienteDTOList.add(new PacienteDTO(e));
-        }
-
-        return pacienteDTOList;
+        return consultaList;
     }
 
-    public void alterar(Paciente paciente){
-        repository.save(paciente);
+    public void alterar(Consulta consulta){
+        repository.save(consulta);
     }
 
     public void excluir(Long id){
         repository.deleteById(id);
     }
 
-    public Optional<Paciente> buscarPorId(Long id){
+    public Optional<Consulta> buscarPorId(Long id){
         return repository.findById(id);
     }
-
-
 }
