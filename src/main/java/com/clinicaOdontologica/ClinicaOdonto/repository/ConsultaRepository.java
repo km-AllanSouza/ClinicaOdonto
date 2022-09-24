d8cae5769d7e4292dd0581d46dbfcd2dd0ed417a
Vinicius <mrVin.dev@gmail.com>
2022-09-23 22:15:26 -0400

    implementando nova verificacao pt1

--- src/main/java/com/clinicaOdontologica/ClinicaOdonto/repository/ConsultaRepository.java
+++ src/main/java/com/clinicaOdontologica/ClinicaOdonto/repository/ConsultaRepository.java
@@ -4,6 +4,11 @@ import com.clinicaOdontologica.ClinicaOdonto.model.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    List<Consulta> findByHorarioAndDataAndIdDentistaId(Time hora, Date data, Long idD);
 }
--- src/main/java/com/clinicaOdontologica/ClinicaOdonto/service/ConsultaService.java
+++ src/main/java/com/clinicaOdontologica/ClinicaOdonto/service/ConsultaService.java
@@ -2,18 +2,12 @@ package com.clinicaOdontologica.ClinicaOdonto.service;
 
 import com.clinicaOdontologica.ClinicaOdonto.model.Consulta;
 import com.clinicaOdontologica.ClinicaOdonto.model.Dentista;
-import com.clinicaOdontologica.ClinicaOdonto.model.Paciente;
-import com.clinicaOdontologica.ClinicaOdonto.model.dto.PacienteDTO;
 import com.clinicaOdontologica.ClinicaOdonto.repository.ConsultaRepository;
 import com.clinicaOdontologica.ClinicaOdonto.repository.DentistaRepository;
-import com.clinicaOdontologica.ClinicaOdonto.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Time;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -29,11 +23,7 @@ public class ConsultaService {
         Long dentistaId = consulta.getIdDentista().getId();
         Optional<Dentista> dentista = dentistaService.findById(dentistaId);
         System.out.println("o nome do dentista é: " + dentista.get().getNome());
-
-    public Consulta salvar(Consulta consulta) throws SQLException {
-        System.out.println(buscarTodos());
-
-        return repository.save(consulta);
+        return consulta;
     }
 
     public List<Consulta> buscarTodos(){
