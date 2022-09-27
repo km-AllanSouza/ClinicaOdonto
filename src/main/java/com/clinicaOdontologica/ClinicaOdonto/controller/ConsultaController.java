52834a2213edbd8dbda7d5c0119d0af0e06247ce
JoelJerrys95 <joeljerrys95@gmail.com>
2022-09-26 21:07:01 -0300

    Logger em endereco e validação de consulta

--- src/main/java/com/clinicaOdontologica/ClinicaOdonto/controller/ConsultaController.java
+++ src/main/java/com/clinicaOdontologica/ClinicaOdonto/controller/ConsultaController.java
@@ -5,8 +5,11 @@ import com.clinicaOdontologica.ClinicaOdonto.model.Horario;
 import com.clinicaOdontologica.ClinicaOdonto.model.Paciente;
 import com.clinicaOdontologica.ClinicaOdonto.model.dto.PacienteDTO;
 import com.clinicaOdontologica.ClinicaOdonto.service.ConsultaService;
+import com.clinicaOdontologica.ClinicaOdonto.service.EnderecoService;
 import com.clinicaOdontologica.ClinicaOdonto.service.HorarioService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,42 +26,37 @@ import java.util.Optional;
 public class ConsultaController {
     @Autowired
     ConsultaService service;
-    @Autowired
-    HorarioService serviceHorario;
+    private static final Logger logger = LogManager.getLogger(EnderecoController.class);
 
     @PostMapping
     public ResponseEntity salvar(@RequestBody Consulta consulta) throws SQLException {
-        List<Horario> listHorarioValido = serviceHorario.buscarHorario(consulta.getHorario());
-        if (!listHorarioValido.isEmpty()){
-            List<Consulta> listHorario = service.verificarHorario(consulta.getHorario(),consulta.getData(), consulta.getIdDentista().getId());
-            if (!listHorario.isEmpty()){
-                return new ResponseEntity<>("Horario indisponivel",HttpStatus.CONFLICT);
-            }
-            return new ResponseEntity(service.salvar(consulta), HttpStatus.OK);
-
-        }
-        return new ResponseEntity<>("O horario informado não condiz com os horarios de marcação do consultorio, consulte o gerente",HttpStatus.OK);
+        logger.info("o metodo salvar foi acionado");
+        return service.salvar(consulta);
     }
 
     @GetMapping
     public ResponseEntity buscarTodos() throws SQLException {
+        logger.info("o metodo buscarTodos foi acionado");
         return new ResponseEntity(service.buscarTodos(),HttpStatus.OK);
     }
 
     @PatchMapping
     public ResponseEntity alterar(@RequestBody Consulta consulta) throws SQLException {
+        logger.info("o metodo alterar foi acionado");
         service.alterar(consulta);
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @DeleteMapping
     public ResponseEntity excluir(@RequestParam Long id) throws SQLException {
+        logger.info("o metodo excluir foi acionado");
         service.excluir(id);
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @RequestMapping(value = "/buscarid", method = RequestMethod.GET)
     public ResponseEntity buscarPorId(@RequestParam("id") Long id) throws SQLException {
+        logger.info("o metodo buscarId foi acionado");
         ObjectMapper mapper = new ObjectMapper();
 
         Optional<Consulta> consultaOptional = service.buscarPorId(id);
